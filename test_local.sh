22c085628045ed5898bdff33a8524286daecddbc
Simran Arora <emailsimran@gmail.com>
2024-06-08 14:49:54 +0000

    update

--- test_local.sh
+++ test_local.sh
@@ -3,6 +3,6 @@
     CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python launch_local.py \
         --batch-size 8 \
         -t boolq \
-        -m hazy-research/cylon/05-21-based-enc-1024d-26l-16h-1024n-30B-tok-v9-shared-projections \
+        -m hazy-research/cylon/05-21-based-my-ablation-1 \
         --limit -1
 
